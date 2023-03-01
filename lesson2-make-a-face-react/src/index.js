31c55d81e186937043872f3d041f00ba63e358f6
reuben <reubenthomas.john2017@gmail.com>
2023-03-01 16:58:03 +0530

    added working webpack config files, npm run dev with react works

--- lesson2-make-a-face-react/src/index.js
+++ lesson2-make-a-face-react/src/index.js
@@ -1,2 +1,38 @@
 import React from 'react';
-console.log(React);
+import ReactDOM from 'react-dom';
+
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 30;
+
+const App = () => (
+    <svg width={width} height={height}>
+        <circle 
+            cx={centerX}
+            cy={centerY}
+            r= {centerY - strokeWidth / 2}
+            fill="yellow"
+            stroke="black"
+            stroke-width={strokeWidth}
+        />
+        <circle 
+            cx={centerX - eyeOffsetX}
+            cy={centerY - eyeOffsetY}
+            r={eyeRadius}
+        />
+        <circle 
+            cx={centerX + eyeOffsetX}
+            cy={centerY - eyeOffsetY}
+            r={eyeRadius}
+        />
+
+    </svg>
+);
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App/>, rootElement);
