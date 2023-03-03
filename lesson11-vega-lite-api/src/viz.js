80713fdae26af2465fe50160e0a5ad5a7bfee143
reuben <reubenthomas.john2017@gmail.com>
2023-03-03 15:16:13 +0530

    increase size and reduce opacity

--- lesson11-vega-lite-api/src/viz.js
+++ lesson11-vega-lite-api/src/viz.js
@@ -1,10 +1,10 @@
 import vl from 'vega-lite-api';
 export const viz = vl
-  .markCircle({ size: 300, opacity: 0.7 })
+  .markCircle({ size: 3000, opacity: 0.2 })
   .encode(
     vl.x().fieldQ('mpg').scale({ zero: false }),
     vl.y().fieldQ('horsepower').scale({ zero: false }),
     vl.color().fieldN('origin'),
-    vl.size().fieldQ('weight'),
+    // vl.size().fieldQ('weight'),
     vl.tooltip().fieldN('name')
   );
